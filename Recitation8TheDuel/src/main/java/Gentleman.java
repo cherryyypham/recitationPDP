f43368daef896f18043faf22646ec46e0c3c087e
cherryyypham <meokkmun@gmail.com>
2025-11-05 23:53:01 -0500

    Finish recit 8

--- Recitation8TheDuel/src/main/java/Gentleman.java
+++
@@ -1,9 +0,0 @@
-/**
- * Our Gentleman class is unable to actually fight!
- * A gentleman would never commit actual violence.
- * It would be so unseemly of him!
- */
-
-public class Gentleman {
-
-}
--- Recitation8TheDuel/src/main/java/Hitman.java
+++
@@ -1,9 +0,0 @@
-/**
- * The Gentleman wants to hire a Hitman who implements the Fight
- * interface. Being able to actually fight is a pretty big
- * deal if someone wants to effectively win a Gentleman's Duel.
- */
-
-public class Hitman {
-
-}
